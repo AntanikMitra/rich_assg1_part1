98016d7ea8bc1b6ce698c4e6cf36bb032f48df5a
AntanikMitra <antanikmitra@hotmail.com>
2016-02-15 19:44:55 -0500

    Second commit

--- views/partials/header.js
+++ views/partials/header.js
@@ -1,19 +1,61 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title><%= title %></title>
-    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/stylesheets/bootstrap-theme.min.css"/>
-    <link rel="stylesheet" href='/stylesheets/style.css' />
-    
-    <script src="/javascripts/jquery-2.2.0.min.js"></script>
-    <script src="/javascripts/bootstrap.min.js"></script>
-  </head>
-<body>
-  <nav class="navbar navbar-default">
-    <ul class="nav navbar-nav">
-        <li><a href="/" title="Home">Father</a></li>
-        <li><a href="/mother" title="mother">Mother</a></li>
-        <li><a href="/nik" title="nik">Antanik</a></li>
-   </ul>
-  </nav>
+<head>
+<title><%= title %></title>
+< link rel= "stylesheet" href= "/stylesheets/bootstrap.min.css" />
+<link rel="stylesheet" href= "/stylesheets/bootstrap-theme.min.css" />
+<link rel="stylesheet" href= '/stylesheets/style.css' />
+<link rel="stylesheet" href= "https://fonts.googleapis.com/icon?family=Material+Icons" >
+<link rel="stylesheet" href= "https://code.getmdl.io/1.1.1/material.indigo-pink.min.css" >
+
+<script defer src= "https://code.getmdl.io/1.1.1/material.min.js" > </script>
+< script src= "/javascripts/jquery-2.2.0.min.js" > </script>
+< script src= "/javascripts/bootstrap.min.js" > </script>
+< /head>
+< body >
+
+
+
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="custom-header mdl-layout__header mdl-layout__header--waterfall">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Brandon Himpfen</span>
+            <div class="mdl-layout-spacer"></div>
+            <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Sidebar Menu</span>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+    </div>
+    <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here --></div>
+    </main>
+</div>
+
+
+
+
+
+
+
+
+
+
+<nav class="navbar navbar-default" >
+<ul class="nav navbar-nav" >
+<li><a href="/" title= "Home" > Father < /a></li >
+<li><a href="/mother" title= "mother" > Mother < /a></li >
+<li><a href="/nik" title= "nik" > Antanik < /a></li >
+</ul>
+< /nav>
