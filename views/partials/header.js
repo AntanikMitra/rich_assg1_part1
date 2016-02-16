3d97db9101ad906427134ce195dc597448a63570
AntanikMitra <antanikmitra@hotmail.com>
2016-02-15 19:56:02 -0500

    Minor change

--- views/partials/header.js
+++ views/partials/header.js
@@ -2,6 +2,9 @@
 <html>
 <head>
 <title><%= title %></title>
+< link rel= "stylesheet" href= "/stylesheets/bootstrap.min.css" />
+<link rel="stylesheet" href= "/stylesheets/bootstrap-theme.min.css" />
+<link rel="stylesheet" href= '/stylesheets/style.css' />
 <link rel="stylesheet" href= "https://fonts.googleapis.com/icon?family=Material+Icons" >
 <link rel="stylesheet" href= "https://code.getmdl.io/1.1.1/material.indigo-pink.min.css" >
 
@@ -13,41 +16,46 @@
 
 
 
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header" >
-<header class="custom-header mdl-layout__header mdl-layout__header--waterfall" >
-<div class="mdl-layout__header-row" >
-<span class="mdl-layout-title" > Antanik Mitra< /span>
-< div class="mdl-layout-spacer" > </div>
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="custom-header mdl-layout__header mdl-layout__header--waterfall">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Brandon Himpfen</span>
+            <div class="mdl-layout-spacer"></div>
+            <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Sidebar Menu</span>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+    </div>
+    <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here --></div>
+    </main>
+</div>
 
-< nav class="mdl-navigation mdl-layout--large-screen-only" >
-<a class="mdl-navigation__link" href= "/" title= "Home" > Anirban < /a>
-< a class="mdl-navigation__link" href= "/mother" title= "mother" > Mousumi < /a>
-< a class="mdl-navigation__link" href= "/nik" title= "nik" > Antanik < /a>
+
+
+
+
+
+
+
+
+
+<nav class="navbar navbar-default" >
+<ul class="nav navbar-nav" >
+<li><a href="/" title= "Home" > Father < /a></li >
+<li><a href="/mother" title= "mother" > Mother < /a></li >
+<li><a href="/nik" title= "nik" > Antanik < /a></li >
+</ul>
 < /nav>
-< /div>
-< /header>
-
-< div class="mdl-layout__drawer" >
-<span class="mdl-layout-title" > Sidebar Menu< /span>
-< nav class="mdl-navigation" >
-<a class="mdl-navigation__link" href= "/" title= "Home" > Anirban < /a>
-< a class="mdl-navigation__link" href= "/mother" title= "mother" > Mousumi < /a>
-< a class="mdl-navigation__link" href= "/nik" title= "nik" > Antanik < /a>
-
-< /nav>
-< /div>
-< main class="mdl-layout__content" >
-<div class="page-content" > <!--Your content goes here -- > </div>
-< /main>
-< /div>
-
-
-
-
-
-
-
-
-
-
-<
