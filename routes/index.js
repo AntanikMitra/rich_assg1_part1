64e692e3c32fbf0dd99a9f49ee37033c996a333d
AntanikMitra <antanikmitra@hotmail.com>
2016-02-15 20:35:49 -0500

    Adding CDN

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Antanik Mitra" });
+  res.render('index', { title: "Home" });
 });
 /* GET about page. */
 router.get('/about', function(req, res, next) {
