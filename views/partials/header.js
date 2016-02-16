4e11576c0cd8ca2a826fda79a0994555978f4385
AntanikMitra <antanikmitra@hotmail.com>
2016-02-15 20:41:57 -0500

    Revert again

--- views/partials/header.js
+++ views/partials/header.js
@@ -1,37 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title><%= title %></title>
-
-<link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css" >
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js" > </script>
-< meta name= "viewport" content= "width=device-width, initial-scale=1.0" />         
-< /head>
-< body >
-
-<nav>
-<div class="nav-wrapper" >
-<a href="/" class="brand-logo"> Antanik </a>
-<a href= "#" data- activates="mobile-demo" class="button-collapse"> <i class="material-icons" > menu < /i></a >
-<ul class="right hide-on-med-and-down" >
-
-<li><a href="/" title= "Home" > Home < /a></li >
-<li><a href="/about" title= "About" > About < /a></li >
-<li><a href="/projects" title= "Projects" > Projects < /a></li >
-<li><a href="/services" title= "Services" > Services < /a></li >
-<li><a href="/contact" title= "Contact" > Contact < /a></li >
-</ul>
-
-< ul class="side-nav" id= "mobile-demo" >
-<li><a href="/" title= "Home" > Home < /a></li >
-<li><a href="/about" title= "About" > About < /a></li >
-<li><a href="/projects" title= "Projects" > Projects < /a></li >
-<li><a href="/services" title= "Services" > Services < /a></li >
-<li><a href="/contact" title= "Contact" > Contact < /a></li >
-</ul>
-</div>
-</nav>
-
-
-
-
+  <head>
+    <title><%= title %></title>
+    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/stylesheets/bootstrap-theme.min.css"/>
+    <link rel="stylesheet" href='/stylesheets/style.css' />
+    
+    <script src="/javascripts/jquery-2.2.0.min.js"></script>
+    <script src="/javascripts/bootstrap.min.js"></script>
+  </head>
+<body>
+  <nav class="navbar navbar-default">
+    <ul class="nav navbar-nav">
+        <li><a href="/" title="Home">Home</a></li>
+        <li><a href="/about" title="About">About</a></li>
+        <li><a href="/projects" title="Projects">Projects</a></li>
+         <li><a href="/services" title="Services">Services</a></li>
+          <li><a href="/contact" title="Contact">Contact</a></li>
+   </ul>
+  </nav>
